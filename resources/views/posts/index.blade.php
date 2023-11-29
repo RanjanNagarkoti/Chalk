c0cd689f55c6ed6f7736c1ec35f0689ec967e78f
Ranjan Nagarkoti <ranjan.ngr2058@gmail.com>
2023-11-29 14:21:13 +0545

    feat: blade master layout

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+@section('content')
+    <section class="d-flex justify-content-between align-items-center my-3">
+        <h1>Posts</h1>
+        <div>
+            <a href="#" class="btn btn-sm btn-primary">New Post</a>
+        </div>
+    </section>
+@endsection
